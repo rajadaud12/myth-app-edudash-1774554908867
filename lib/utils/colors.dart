7fb09ca6e51c65cd14ff650916fff7baee5c9ec1
Daud Bin Nasar <64163448+rajadaud12@users.noreply.github.com>
2026-03-27 00:55:18 +0500

    Update lib/utils/colors.dart via MYTH Build System

---
+++ lib/utils/colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primary = Color(0xFF6C63FF);
+  static Color secondary = Color(0xFFFF6584);
+  static Color accent = Color(0xFF43D9A2);
+  static Color accentOrange = Color(0xFFFFB347);
+  static Color accentBlue = Color(0xFF38BDF8);
+  static Color background = Color(0xFFF4F6FB);
+  static Color cardWhite = Color(0xFFFFFFFF);
+  static Color textDark = Color(0xFF1E1E2D);
+  static Color textGrey = Color(0xFF9B9BAD);
+  static Color textLight = Color(0xFFFFFFFF);
+  static Color divider = Color(0xFFE8EAF0);
+  static Color success = Color(0xFF43D9A2);
+  static Color warning = Color(0xFFFFB347);
+  static Color error = Color(0xFFFF6584);
+  static Color navBarBg = Color(0xFF1E1E2D);
+  static Color sidebarBg = Color(0xFF2A2A3D);
+  static Color gradientStart = Color(0xFF6C63FF);
+  static Color gradientEnd = Color(0xFF43D9A2);
+  static Color statCard1 = Color(0xFF6C63FF);
+  static Color statCard2 = Color(0xFFFF6584);
+  static Color statCard3 = Color(0xFF43D9A2);
+  static Color statCard4 = Color(0xFFFFB347);
+}
